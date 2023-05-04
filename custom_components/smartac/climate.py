467efd238d5eb627d81b3f1576d5776dbb3a14a4
ryan he <ryanh7@163.com>
2023-05-05 05:08:36 +0800

    修复函数调用

--- custom_components/smartac/climate.py
+++ custom_components/smartac/climate.py
@@ -298,7 +298,7 @@ class SmartACClimate(ClimateEntity, RestoreEntity):
         if not self._hvac_mode.lower() == HVAC_MODE_OFF:
             await self.send_command()
 
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_hvac_mode(self, hvac_mode):
         """Set operation mode."""
@@ -308,7 +308,7 @@ class SmartACClimate(ClimateEntity, RestoreEntity):
             self._last_on_operation = hvac_mode
 
         await self.send_command()
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_fan_mode(self, fan_mode):
         """Set fan mode."""
@@ -316,7 +316,7 @@ class SmartACClimate(ClimateEntity, RestoreEntity):
 
         if not self._hvac_mode.lower() == HVAC_MODE_OFF:
             await self.send_command()
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_swing_mode(self, swing_mode):
         """Set swing mode."""
@@ -324,7 +324,7 @@ class SmartACClimate(ClimateEntity, RestoreEntity):
 
         if not self._hvac_mode.lower() == HVAC_MODE_OFF:
             await self.send_command()
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_turn_off(self):
         """Turn off."""
@@ -370,7 +370,7 @@ class SmartACClimate(ClimateEntity, RestoreEntity):
             return
 
         self._async_update_temp(new_state)
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def _async_humidity_sensor_changed(self, entity_id, old_state, new_state):
         """Handle humidity sensor changes."""
@@ -378,7 +378,7 @@ class SmartACClimate(ClimateEntity, RestoreEntity):
             return
 
         self._async_update_humidity(new_state)
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def _async_power_sensor_changed(self, entity_id, old_state, new_state):
         """Handle power sensor changes."""
@@ -395,13 +395,13 @@ class SmartACClimate(ClimateEntity, RestoreEntity):
             else:
                 self._hvac_mode = STATE_ON
 
-            await self.async_update_ha_state()
+            self.async_write_ha_state()
 
         if new_state.state == STATE_OFF:
             self._on_by_remote = False
             if self._hvac_mode != HVAC_MODE_OFF:
                 self._hvac_mode = HVAC_MODE_OFF
-            await self.async_update_ha_state()
+            self.async_write_ha_state()
 
     @callback
     def _async_update_temp(self, state):
